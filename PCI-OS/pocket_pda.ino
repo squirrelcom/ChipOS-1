5017c2d2088b87ac442d714b0e03a0fb1880b6cf
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-07-30 11:18:33 +0100

    Add files via upload

---
+++ PCI-OS/pocket_pda.ino
@@ -0,0 +1,38 @@
+#include "data.h"
+#include "logic.h"
+#include "visual.h"
+#include <Arduboy2.h>
+
+static PdaOS OS;
+static Arduboy2 A;
+
+static void intro(void) {
+  for (char i = -8; i < 28; i = i + 2) {
+    A.clear();
+    A.setCursor(34, i);
+    A.print("Pocket PDA");
+    A.setCursor(46, i + 8);
+    A.print("V0.8.0");
+    delay(20);
+    A.display();
+  }
+  delay(1000);
+}
+
+void setup() {
+  Serial.begin(9600);
+  A.begin();
+  A.initRandomSeed();
+  A.setFrameRate(FPS);
+  intro();
+  OS.arduboy = &A;
+  initLogic(&OS);
+}
+
+void loop() {
+  if (!(A.nextFrame()))
+    return;
+  A.clear();
+  run(&OS);
+  A.display();
+}
---
+++ PCI-OS/visual.h
@@ -0,0 +1,9 @@
+#ifndef __VISUAL_H__
+#define __VISUAL_H__
+
+extern const uint8_t SPRITE_MENU0[];
+extern const uint8_t SPRITE_MENU1[];
+extern const uint8_t SPRITE_UP[];
+extern const uint8_t SPRITE_DOWN[];
+
+#endif /* __VISUAL_H__ */
