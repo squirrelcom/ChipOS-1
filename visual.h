20a886332ae8e225cf03e46f8a799d694586816f
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-07-30 11:18:01 +0100

    Delete visual.h

--- visual.h
+++
@@ -1,9 +0,0 @@
-#ifndef __VISUAL_H__
-#define __VISUAL_H__
-
-extern const uint8_t SPRITE_MENU0[];
-extern const uint8_t SPRITE_MENU1[];
-extern const uint8_t SPRITE_UP[];
-extern const uint8_t SPRITE_DOWN[];
-
-#endif /* __VISUAL_H__ */
